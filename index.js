8857e76719cf73b624c29d38a6207f9b5cac9341
InfernoDuck <65702538+InfernoDuck@users.noreply.github.com>
2020-05-22 08:55:54 +1200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.login('Mjk1Nzk5MTMwNjQxMzk5ODA4.Xm9Ptw.JZfFfGGYxJfPgqiiXlLi-Rm8DZs');
+
+client.on('ready', ()=>{
+    console.log("lmao");
+});
