9472f70294a0688840f4b6ed03eb27be3836a936
InfernoDuck <65702538+InfernoDuck@users.noreply.github.com>
2020-05-27 17:21:21 +1200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,90 @@
 const Discord = require('discord.js');
+const sql = require('sqlite3').verbose();
 const client = new Discord.Client();
 
-client.login('Mjk1Nzk5MTMwNjQxMzk5ODA4.Xm9Ptw.JZfFfGGYxJfPgqiiXlLi-Rm8DZs');
+client.login('NzA3OTAzOTI2MzQ2OTczMjY0.XsrhIA.6qdIAwXTEnRBdu_Fr51EfHNEwWE');
 
-client.on('ready', ()=>{
+
+client.on('ready', ()=>
+{
     console.log("lmao");
+});
+
+client.on('message', (message)=>
+{
+    if(message.author == client.user)
+    {
+        return;
+    }
+
+    let command;
+    let parameter;
+    if(message.content.substring(0, 1) == "!")
+    {
+        if(message.content.search(" ") == -1)
+        {
+            if(message.content.substring(1) != "")
+            {
+                command = message.content.substring(1);
+            }
+            else
+            {
+                message.reply("no command");
+            }
+        }
+        else
+        {
+            parameter = message.content.substring(message.content.search(" ") + 1);
+            command = message.content.substring(1, message.content.search(" "));
+        }
+    }
+    if(command){
+        switch(command)
+        {
+            case "delete":
+                message.reply("deleting " + parameter + " messages");
+                break;
+            case "hello":
+                message.reply("hi");
+                break;
+            case "say":
+                message.channel.send(parameter);
+                break;
+            case "db":
+                
+                var db = new sql.Database('./db/test.db', (err) => {
+                    if (err) 
+                    {
+                        return console.error(err.message);
+                    }
+                    console.log('Connected to the in-memory SQlite database.');
+                });
+
+                db.serialize(function()
+                {
+                    db.all(parameter, (err, result)=>{
+                        if(err)
+                        {
+                            message.reply(err.message);
+                        }
+                        else
+                        {
+                            message.reply(result);
+                        }
+                    });
+                });
+
+                db.close((err) => {
+                    if (err) {
+                    return message.reply(err.message);
+                    }
+                    console.log('Close the database connection.');
+                });
+                break;
+            default:
+                message.reply("not a valid command");
+                break;
+
+        }
+    }
 });
